e58e58b2c4e73eedc1c9d626203f158ded016663
joylei <leingliu@gmail.com>
2021-04-11 23:27:32 +0800

    do not leak RawTag

--- plctag-async/src/op.rs
+++ plctag-async/src/op.rs
@@ -1,36 +1,76 @@
 use std::sync::Arc;
 
 use crate::{cell::OnceCell, Result, TagRef};
-use plctag::{event::Event, GetValue, RawTag, SetValue};
+use plctag::{event::Event, GetValue, RawTag, SetValue, TagId};
+
+pub trait AsRaw {
+    fn as_raw(&self) -> &RawTag;
+}
 
 #[async_trait]
-pub trait AsyncTag: AsRef<RawTag> {
+pub trait AsyncTag: AsRaw {
+    #[inline(always)]
+    fn id(&self) -> TagId {
+        self.as_raw().id()
+    }
+
+    #[inline(always)]
+    fn size(&self) -> Result<u32> {
+        Ok(self.as_raw().size()?)
+    }
+
+    #[inline(always)]
+    fn elem_count(&self) -> Result<i32> {
+        Ok(self.as_raw().elem_count()?)
+    }
+
+    #[inline(always)]
+    fn elem_size(&self) -> Result<i32> {
+        Ok(self.as_raw().elem_size()?)
+    }
+
+    #[inline(always)]
+    fn get_value<T: GetValue + Default>(&self, byte_offset: u32) -> Result<T> {
+        Ok(self.as_raw().get_value(byte_offset)?)
+    }
+
+    #[inline(always)]
+    fn set_value<T: SetValue>(&self, byte_offset: u32, value: T) -> Result<()> {
+        self.as_raw().set_value(byte_offset, value)?;
+        Ok(())
+    }
+
     #[inline(always)]
     async fn read(&self) -> Result<()> {
-        let mut op = Operation::new(self.as_ref());
+        let mut op = Operation::new(self.as_raw());
         op.run(true).await
     }
 
     #[inline(always)]
     async fn write(&self) -> Result<()> {
-        let mut op = Operation::new(self.as_ref());
+        let mut op = Operation::new(self.as_raw());
         op.run(false).await
     }
 
     #[inline(always)]
     async fn read_value<T: GetValue + Default>(&self, offset: u32) -> Result<T> {
         self.read().await?;
-        Ok(self.as_ref().get_value(offset)?)
+        Ok(self.as_raw().get_value(offset)?)
     }
 
     #[inline(always)]
     async fn write_value<T: SetValue + Send>(&self, offset: u32, value: T) -> Result<()> {
-        self.as_ref().set_value(offset, value)?;
+        self.as_raw().set_value(offset, value)?;
         self.write().await?;
         Ok(())
     }
 }
-
+impl AsRaw for TagRef<'_> {
+    #[inline(always)]
+    fn as_raw(&self) -> &RawTag {
+        &self.tag
+    }
+}
 impl AsyncTag for TagRef<'_> {}
 
 /// ensures that pending operation get aborted if not successful
