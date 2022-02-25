a3928caf2fa1dbe025b5c434f428383d7bb3a25d
joylei <leingliu@gmail.com>
2022-02-25 20:06:06 +0800

    update file header

--- examples/async-read-write.rs
+++ examples/async-read-write.rs
@@ -1,7 +1,7 @@
 // plctag-rs
 //
 // a rust wrapper of libplctag, with rust style APIs and useful extensions.
-// Copyright: 2020-2021, Joylei <leingliu@gmail.com>
+// Copyright: 2022, Joylei <leingliu@gmail.com>
 // License: MIT
 
 use plctag::futures::{Error, TagEntry};
