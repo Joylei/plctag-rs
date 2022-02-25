ec23ee3a6bb2a245e9f2a14ddcc6145cad3f4243
joylei <leingliu@gmail.com>
2022-02-25 11:42:16 +0800

    fix async example

--- examples/async-read-write.rs
+++ examples/async-read-write.rs
@@ -4,7 +4,7 @@
 // Copyright: 2020-2021, Joylei <leingliu@gmail.com>
 // License: MIT
 
-use plctag::futures::{AsyncTag, Error, TagEntry};
+use plctag::futures::{Error, TagEntry};
 
 use tokio::runtime;
 
@@ -12,14 +12,13 @@ fn main() {
     let rt = runtime::Runtime::new().unwrap();
     let res: Result<_, Error> = rt.block_on(async {
         let path="protocol=ab-eip&plc=controllogix&path=1,0&gateway=192.168.1.120&name=MyTag1&elem_count=1&elem_size=16"; // YOUR TAG DEFINITION
-        let tag = TagEntry::create(path).await?;
-        let tag_ref = tag.get().await?;
+        let mut tag = TagEntry::create(path).await?;
         let offset = 0;
-        let value: u16 = tag_ref.read_value(offset).await?;
+        let value: u16 = tag.read_value(offset).await?;
         println!("tag value: {}", value);
 
         let value = value + 10;
-        tag_ref.write_value(offset, value).await?;
+        tag.write_value(offset, value).await?;
         Ok(())
     });
     res.unwrap();
