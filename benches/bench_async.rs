4fe51cc2556e1028c49ab5fe8c02be2646319f44
joylei <leingliu@gmail.com>
2023-01-14 12:12:01 +0800

    use RefCell instead of UnsafeCell in bench

--- benches/bench_async.rs
+++ benches/bench_async.rs
@@ -1,6 +1,6 @@
 use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
 use plctag::futures::AsyncTag;
-use std::{cell::UnsafeCell, sync::Arc};
+use std::{cell::RefCell, sync::Arc};
 
 fn bench_read(c: &mut Criterion) {
     c.bench_function("async read", |b| {
@@ -11,12 +11,12 @@ fn bench_read(c: &mut Criterion) {
         let entry = rt.block_on(async {
             let path="protocol=ab-eip&plc=controllogix&path=1,0&gateway=192.168.0.83&name=Car_Pos[1]&elem_count=1";
             let tag = AsyncTag::create(path).await.unwrap();
-            Arc::new(UnsafeCell::new(tag))
+            Arc::new(RefCell::new(tag))
         });
         b.to_async(rt).iter_batched(
             || entry.clone(),
             |entry| async move {
-                let tag = unsafe {&mut *entry.get()};
+                let tag = &mut* entry.borrow_mut();
                 let _value: i32 = tag.read_value(0).await.unwrap();
             },
             BatchSize::PerIteration,
