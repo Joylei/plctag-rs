7ac3fb0d31eab8e6285741e321a5c2a8a53b0b32
joylei <leingliu@gmail.com>
2023-05-03 17:57:40 +0800

    upgrade syn 2

--- crates/derive/src/shared.rs
+++ crates/derive/src/shared.rs
@@ -6,7 +6,7 @@
 
 use proc_macro2::Span;
 use proc_macro_crate::{crate_name, FoundCrate};
-use syn::{Attribute, Data, DataStruct, Fields, Ident, Lit, Meta, NestedMeta, Type};
+use syn::{Attribute, Data, DataStruct, Fields, Ident, LitInt, LitStr, Type};
 
 pub fn get_crate() -> syn::Result<Ident> {
     let plctag = match crate_name("plctag").or_else(|_| crate_name("plctag-core")) {
@@ -33,7 +33,7 @@ pub fn get_fields(data: Data, ctx: &Context) -> syn::Result<Vec<(Ident, Type, Ta
             let attrs: Vec<_> = f
                 .attrs
                 .iter()
-                .filter(|attr| attr.path.is_ident("tag"))
+                .filter(|attr| attr.path().is_ident("tag"))
                 .collect();
             assert!(!attrs.is_empty());
             let offset = match attrs.len() {
@@ -64,102 +64,52 @@ pub fn get_fields(data: Data, ctx: &Context) -> syn::Result<Vec<(Ident, Type, Ta
 }
 
 fn get_tag_attr(attr: &Attribute, ctx: &Context) -> syn::Result<TagAttr> {
-    let meta = attr.parse_meta()?;
-    //tag()
-    let meta_list = match meta {
-        Meta::List(list) => list,
-        _ => {
-            return Err(syn::Error::new_spanned(
-                meta,
-                "bad usage, please refer to tag attribute",
-            ))
-        }
-    };
-
-    let nested = &meta_list.nested;
     let mut offset = None;
     let mut size = None;
     let mut encode_fn = None;
     let mut decode_fn = None;
-    for item in nested {
-        let name_value = match item {
-            NestedMeta::Meta(Meta::NameValue(nv)) => nv,
-            _ => {
-                return Err(syn::Error::new_spanned(
-                    nested,
-                    "expected `offset = \"<value>\"` or `size = \"<value>\"`",
-                ))
-            }
-        };
 
-        if name_value.path.is_ident("offset") {
-            match &name_value.lit {
-                Lit::Int(s) => {
-                    if offset.is_some() {
-                        return Err(syn::Error::new_spanned(
-                            s,
-                            "redundant definition for offset",
-                        ));
-                    }
-                    offset = Some(s.base10_parse()?);
-                }
-                lit => return Err(syn::Error::new_spanned(lit, "expected int literal")),
+    attr.parse_nested_meta(|item| {
+        if item.path.is_ident("offset") {
+            if offset.is_some() {
+                return Err(item.error("redundant definition for offset"));
             }
-        } else if name_value.path.is_ident("size") {
-            match &name_value.lit {
-                Lit::Int(s) => {
-                    if size.is_some() {
-                        return Err(syn::Error::new_spanned(s, "redundant definition for size"));
-                    }
-                    size = Some(s.base10_parse()?);
-                }
-                lit => return Err(syn::Error::new_spanned(lit, "expected int literal")),
+            let lit: LitInt = item.value()?.parse()?;
+            offset = Some(lit.base10_parse()?);
+        } else if item.path.is_ident("size") {
+            if size.is_some() {
+                return Err(item.error("redundant definition for size"));
             }
-        } else if name_value.path.is_ident("encode_fn") {
-            match &name_value.lit {
-                Lit::Str(s) => {
-                    if encode_fn.is_some() {
-                        return Err(syn::Error::new_spanned(
-                            s,
-                            "redundant definition for encode_fn",
-                        ));
-                    }
-                    let expr: syn::ExprPath = s.parse()?;
-                    encode_fn = Some(expr);
-                }
-                lit => return Err(syn::Error::new_spanned(lit, "expected Str literal")),
+            let lit: LitInt = item.value()?.parse()?;
+            size = Some(lit.base10_parse()?);
+        } else if item.path.is_ident("encode_fn") {
+            if encode_fn.is_some() {
+                return Err(item.error("redundant definition for encode_fn"));
             }
-        } else if name_value.path.is_ident("decode_fn") {
-            match &name_value.lit {
-                Lit::Str(s) => {
-                    if decode_fn.is_some() {
-                        return Err(syn::Error::new_spanned(
-                            s,
-                            "redundant definition for decode_fn",
-                        ));
-                    }
-                    let expr: syn::ExprPath = s.parse()?;
-                    decode_fn = Some(expr);
-                }
-                lit => return Err(syn::Error::new_spanned(lit, "expected Str literal")),
+            let lit: LitStr = item.value()?.parse()?;
+            let expr: syn::ExprPath = lit.parse()?;
+            encode_fn = Some(expr);
+        } else if item.path.is_ident("decode_fn") {
+            if decode_fn.is_some() {
+                return Err(item.error("redundant definition for decode_fn"));
             }
+            let lit: LitStr = item.value()?.parse()?;
+            let expr: syn::ExprPath = lit.parse()?;
+            decode_fn = Some(expr);
         } else {
-            // Could also silently ignore the unexpected attribute by returning `Ok(None)`
-            return Err(syn::Error::new_spanned(
-                &name_value.path,
-                "unknown tag attribute",
-            ));
+            return Err(item.error("unknown tag attr"));
         }
-    }
+        Ok(())
+    })?;
 
     if ctx.is_encode && encode_fn.is_none() && offset.is_none() {
         return Err(syn::Error::new_spanned(
-            &meta_list.path,
+            attr.path(),
             "at least one of tag attribute `offset`, `encode_fn` is required",
         ));
     } else if !ctx.is_encode && decode_fn.is_none() && offset.is_none() {
         return Err(syn::Error::new_spanned(
-            &meta_list.path,
+            attr.path(),
             "at least one of tag attribute `offset`, `decode_fn` is required",
         ));
     }
