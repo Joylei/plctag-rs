4b8bb2daf5fcc9a9d8b53e33dcae0ec8a7a5508d
joylei <leingliu@gmail.com>
2023-10-17 22:02:01 +0800

    add tests for encode_fn/decode_fn

--- crates/derive/tests/test_derive.rs
+++ crates/derive/tests/test_derive.rs
@@ -15,6 +15,22 @@ struct MyUDT {
     b: u32,
 }
 
+#[derive(Debug, Default, Decode, Encode)]
+struct MyUDT2 {
+    #[tag(offset = 0)]
+    a: u32,
+    #[tag(encode_fn = "my_encode", decode_fn = "my_decode")]
+    b: u32,
+}
+
+fn my_encode(v: &u32, tag: &RawTag, offset: u32) -> plctag_core::Result<()> {
+    tag.set_u32(offset + 4, *v)
+}
+
+fn my_decode(tag: &RawTag, offset: u32) -> plctag_core::Result<u32> {
+    tag.get_u32(offset + 4).map(|v| v + 1)
+}
+
 #[test]
 fn test_derive() {
     let tag = RawTag::new("make=system&family=library&name=debug&debug=4", 100).unwrap();
@@ -23,4 +39,7 @@ fn test_derive() {
     let udt: MyUDT = tag.get_value(0).unwrap();
     assert_eq!(udt.a, 4);
     assert_eq!(udt.b, 0);
+    let udt2: MyUDT2 = tag.get_value(0).unwrap();
+    assert_eq!(udt2.a, 4);
+    assert_eq!(udt2.b, 0);
 }
